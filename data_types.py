3126db4fdf4f6b7c4dd8013cfd88088167674c5a
AndrewS619 <72218461+AndrewS619@users.noreply.github.com>
2020-10-14 15:10:56 -0700

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,14 @@
+import numpy as np #import numpy
+
+#integers
+i = 10 #integer
+print("the type of i is ",type(i))  #print out the type
+
+a_i = np.zeros(i,dtype=int)  #an array of ints
+print("The type of a_i is ",type(a_i))  #print out the type
+print("The type of a_i[0] is ", type(a_i[0]))  #print out the type
+
+
+#floats
+x = 119.0  #floating point number
+print()
---
+++ while.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+i = 0			#define an integer
+
+while (i<119):			# while i is less than 119, do the following
+	print(i)			#print the current value of i
+	i += 10				#add 10 to i
